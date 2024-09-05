4a051c3f590a756897af42352029517f93b9ee94
availe <94802525+availe@users.noreply.github.com>
2024-09-05 13:35:57 -0400

    navbar drawer init

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,8 +1,21 @@
 "use client";
 
 import { useMediaQuery } from "@/hooks/use-media-query";
+import { Drawer, DrawerContent, DrawerTrigger } from "./ui/drawer";
+import { MenuIcon } from "lucide-react";
 
 export default function NavBar() {
-    const isDesktop = useMediaQuery("(min-width: 768px)");
-    return isDesktop ? <div>isDesktop</div> : <div>isMobile</div>;
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  return isDesktop ? (
+    <div>isDesktop</div>
+  ) : (
+    <Drawer direction="right">
+      <DrawerTrigger>
+        <MenuIcon />
+      </DrawerTrigger>
+      <DrawerContent>
+        Placeholder Link
+      </DrawerContent>
+    </Drawer>
+  );
 }
