5cd39588c2c7f259fde9e0d5de4081c9bb7e1933
availe <94802525+availe@users.noreply.github.com>
2024-09-06 13:59:08 -0400

    &apos;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="min-h-screen bg-red-200 flex flex-col justify-center bg-[url('/flat-mountains.svg')] bg-no-repeat bg-center bg-cover">
       <section className="text-center flex flex-col gap-4">
         <div className="flex flex-col gap-2">
-        <div className="uppercase">Hello, I'm Rafael Diaz</div>
+        <div className="uppercase">Hello, I&apos;m Rafael Diaz</div>
         <div>Prev. Software Engineering Intern at Newmark</div>
         </div>
         <Link href="/about">
