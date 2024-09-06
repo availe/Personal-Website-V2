79dca1506f86f8460cba6fb1a09da17b0aab49a5
availe <94802525+availe@users.noreply.github.com>
2024-09-06 12:20:37 -0400

    hero bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <section className="text-center">
+    <main className="min-h-screen bg-red-200 flex flex-col justify-center bg-[url('/flat-mountains.svg')] bg-no-repeat bg-center bg-cover">
+      <section className="text-center flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
         <div className="uppercase">Hello, I'm Rafael Diaz</div>
         <div>Prev. Software Engineering Intern at Newmark</div>
+        </div>
         <Link href="/about">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           About Me
