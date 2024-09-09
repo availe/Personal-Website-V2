b2f7094225d9f0ca4546c8e10a0ccf215115410d
availe <94802525+availe@users.noreply.github.com>
2024-09-09 17:36:03 -0400

    Update bento-grid.tsx

--- src/components/grids/bento-grid.tsx
+++ src/components/grids/bento-grid.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { Button } from "../ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "../ui/card";
 
 interface Project {
   name: string;
@@ -13,13 +21,40 @@ interface BentoGridProps {
 }
 
 const BentoGrid: React.FC<BentoGridProps> = ({ projects = [] }) => {
+  const uniqueCategories = Array.from(
+    new Set(projects.map((project) => project.category))
+  );
+
   return (
     <section className="flex flex-col">
-      {projects.map((project) => (
-        <Button key={project.name} variant="outline">
-          {project.category}
-        </Button>
-      ))}
+      <div className="flex justify-center gap-6">
+        {uniqueCategories.map((category) => (
+          <Button key={category} variant="outline">
+            {category}
+          </Button>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mt-4">
+        {projects.map((project, index) => (
+          <Card key={index} className="p-4">
+            <CardHeader>
+              <CardTitle>{project.name}</CardTitle>
+              <CardDescription>More about {project.name}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <img
+                src={project.image}
+                alt={project.name}
+                style={{ width: "100%" }}
+              />
+              <p>{project.category}</p>
+            </CardContent>
+            <CardFooter>
+              <a href={project.link}>Learn More</a>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </section>
   );
 };
