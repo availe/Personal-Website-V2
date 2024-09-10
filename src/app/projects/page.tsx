da90fd4f30a94d6f1d926e4758645250c7b883fe
availe <94802525+availe@users.noreply.github.com>
2024-09-09 20:02:36 -0400

    project layout

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,34 +1,40 @@
-import BentoGrid from "@/components/grids/bento-grid";
-
-const projects = [
-  {
-    name: "Project 1",
-    image: "/next.svg",
-    category: "Web",
-    link: "#"
-  },
-  {
-    name: "Project 2",
-    image: "/next.svg",
-    category: "Data",
-    link: "#"
-  },
-  {
-    name: "Project 3",
-    image: "/next.svg",
-    category: "Data",
-    link: "#"
-  },
-  {
-    name: "Project 4",
-    image: "/next.svg",
-    category: "Programming",
-    link: "#"
-  },
-];
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardFooter,
+} from "@/components/ui/card";
+import Image from "next/image";
 
 export default function ProjectsPage() {
-    return (
-      <BentoGrid projects={projects}/>
-    );
-  }
+  return (
+    <Card className="flex flex-col">
+      <CardHeader className="flex w-full justify-center p-4">
+        <h2 className="text-2xl">Projects</h2>
+      </CardHeader>
+      <CardContent className="flex">
+        <div className="w-1/2 flex justify-center">
+          <figure>
+            <Image
+              src="/expense-tracker-mockup.svg"
+              alt="Expense Tracker Mockup"
+              width="572"
+              height="272"
+              style={{ objectFit: "contain" }}
+            />
+            <figcaption className="text-center mt-2">
+              Figma mockup of project
+            </figcaption>
+          </figure>
+        </div>
+        <div className="w-1/2 flex flex-col p-4">
+          <h2 className="text-2xl">Expense Tracker</h2>
+          <p>Details.</p>
+        </div>
+      </CardContent>
+      <CardFooter className="flex justify-center p-4">
+        <p>Link.</p>
+      </CardFooter>
+    </Card>
+  );
+}
