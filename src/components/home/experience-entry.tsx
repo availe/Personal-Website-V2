1e23b4a5d9f0d445e6778c52036e8c985d1e2f09
availe <94802525+availe@users.noreply.github.com>
2024-09-10 23:30:07 -0400

    updated button style home

--- src/components/home/experience-entry.tsx
+++ src/components/home/experience-entry.tsx
@@ -1,3 +1,5 @@
+import { Button } from "../ui/button";
+
 interface ExperienceEntryProps {
   link: string;
   name: string;
@@ -25,11 +27,11 @@ const ExperienceEntry: React.FC<ExperienceEntryProps> = ({
       </div>
       <p>{description}</p>
       <div className="flex justify-center md:justify-end pt-2">
-        <button className="w-[10rem] bg-blue-600 hover:bg-blue-500 text-white py-1 px-2 rounded">
+        <Button variant="outline">
           <a href={link} target="_blank" rel="noopener noreferrer">
             {callToAction}
           </a>
-        </button>
+        </Button>
       </div>
     </div>
   );
