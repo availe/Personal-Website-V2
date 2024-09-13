c3ea765998e981a8d24d204a2221187c658e1e54
availe <94802525+availe@users.noreply.github.com>
2024-09-13 17:47:11 -0400

    Update project-entry.tsx

--- src/components/project-entry/project-entry.tsx
+++ src/components/project-entry/project-entry.tsx
@@ -2,29 +2,31 @@ import { ImageSection } from "./image-section";
 import { ProjectEntryProps } from "./project-entry-props";
 import { TextSection } from "./text-section";
 
-const ProjectEntry: React.FC<ProjectEntryProps > = ({
+const ProjectEntry: React.FC<ProjectEntryProps> = ({
   imageSrc,
   imageAlt,
   projectName,
   projectDescription,
   projectDetails,
   codeUrl,
-  imageLeft = true,
+  imageLeft,
   isInternal,
 }) => (
-    <section
-      aria-labelledby="project-title"
-      className="container mx-auto grid grid-cols-1 lg:grid-cols-2 justify-items-center items-center"
-    >
-      <ImageSection src={imageSrc} alt={imageAlt} imageLeft={imageLeft} />
-      <TextSection
-        title={projectName}
-        description={projectDescription}
-        details={projectDetails}
-        codeUrl={codeUrl}
-        isInternal={isInternal}
-      />
-    </section>
+  <section
+    aria-labelledby="project-title"
+    className={`container mx-auto grid grid-cols-1 lg:grid-cols-2 justify-items-center items-center ${
+      imageLeft ? "lg:order-1" : "lg:order-2"
+    }`}
+  >
+    <ImageSection src={imageSrc} alt={imageAlt} imageLeft={imageLeft} />
+    <TextSection
+      title={projectName}
+      description={projectDescription}
+      details={projectDetails}
+      codeUrl={codeUrl}
+      isInternal={isInternal}
+    />
+  </section>
 );
 
 export default ProjectEntry;
