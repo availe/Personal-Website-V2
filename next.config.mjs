ea3b529e590e464eee63f7f42f715a315a14e8f5
availe <94802525+availe@users.noreply.github.com>
2024-09-14 14:23:14 -0400

    deployment scripts

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,6 @@ const nextConfig = {
       {
         source: '/(.*?)',
         headers: [
-          { key: 'Content-Security-Policy', value: "default-src 'self'; img-src 'self' https:; script-src 'self' 'unsafe-inline' https:; style-src 'self' 'unsafe-inline' https:; object-src 'none'" },
-          { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
-          { key: 'X-Frame-Options', value: 'DENY' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'Referrer-Policy', value: 'no-referrer-when-downgrade' },
-          { key: 'Permissions-Policy', value: 'geolocation=(), camera=(), microphone=()' },
-          { key: 'Expect-CT', value: 'max-age=86400, enforce' },
-          { key: 'X-XSS-Protection', value: '1; mode=block' },
           { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
           { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
         ],
