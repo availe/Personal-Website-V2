37487d2837b130965518c5d393394aeac9529ad2
availe <94802525+availe@users.noreply.github.com>
2024-09-14 14:25:34 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
-    return [
-      {
-        source: '/(.*?)',
-        headers: [
-          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
-          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
-        ],
-      },
-    ];
+    if (process.env.NODE_ENV === "development") {
+      return [
+        {
+          source: "/(.*?)",
+          headers: [
+            { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+            { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          ],
+        },
+      ];
+    }
+    return [];
   },
 };
 
