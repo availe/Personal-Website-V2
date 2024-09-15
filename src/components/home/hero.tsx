72d00e752e75ca5453fefe2dfa9c0113d72d7895
availe <94802525+availe@users.noreply.github.com>
2024-09-15 16:07:30 -0400

    Revert "preload hero background"
    This reverts commit 42bf353521a7e6b32cc6e1f16426ae9ec67b6772.

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,36 +1,23 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
-import Head from "next/head";
 
 const Hero = () => {
-  const backgroundImageUrl = "/flat-mountains.svg";
-
   return (
-    <>
-      <Head>
-        <link rel="preload" href={backgroundImageUrl} as="image" />
-      </Head>
-      <main className="min-h-[85svh] bg-[url('/flat-mountains.svg')] bg-cover bg-center flex flex-col justify-center">
-        <section className="text-center flex flex-col gap-6 md:gap-8 md:mt-[-10svh]">
-          <div className="flex flex-col gap-2">
-            <h1 className="text-3xl md:text-5xl">
-              Hello, I&apos;m Rafael Diaz
-            </h1>
-            <p className="md:text-xl">
-              Prev. Software Engineering Intern at Newmark
-            </p>
-          </div>
-          <Link href="/resume">
-            <Button
-              variant="default"
-              className="bg-blue-600 hover:bg-blue-500 md:text-lg"
-            >
-              View Resume
-            </Button>
-          </Link>
-        </section>
-      </main>
-    </>
+    <main className="min-h-[85svh] bg-[url('/flat-mountains.svg')] bg-cover bg-center flex flex-col justify-center">
+      <section className="text-center flex flex-col gap-6 md:gap-8 md:mt-[-10svh]">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-3xl md:text-5xl">Hello, I&apos;m Rafael Diaz</h1>
+          <p className="md:text-xl">
+            Prev. Software Engineering Intern at Newmark
+          </p>
+        </div>
+        <Link href="/resume">
+          <Button variant="default" className="bg-blue-600 hover:bg-blue-500 md:text-lg">
+            View Resume
+          </Button>
+        </Link>
+      </section>
+    </main>
   );
 };
 
