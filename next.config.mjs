d6bbb5991ba8abd75fbb12e1e83b7e734f5875f1
availe <94802525+availe@users.noreply.github.com>
2024-09-15 17:12:59 -0400

    Revert "security headers"
    This reverts commit 08d20964834b4eed70282f79bde16bafb9e97f75.

--- next.config.mjs
+++ next.config.mjs
@@ -7,18 +7,6 @@ const nextConfig = {
         headers: [
           { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
           { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; script-src 'self'; worker-src 'self'; frame-ancestors 'none';",
-          },
-        ],
-      },
-      {
-        source: "/Escape HTML/:path*",
-        headers: [
-          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
         ],
       },
     ];
