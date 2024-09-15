2cf1c972f1f057ce419d1c2401f3dceeedbf060b
availe <94802525+availe@users.noreply.github.com>
2024-09-15 18:20:16 -0400

    ssr render

--- src/app/projects/escape/page.tsx
+++ src/app/projects/escape/page.tsx
@@ -1,7 +1,15 @@
+import { GetServerSideProps } from "next";
+
 const Escape = () => {
-    return (
-        <iframe src="/escape-html/game.html" className="h-screen w-screen"></iframe>
-    );
-}
+  return (
+    <iframe src="/escape-html/game.html" className="h-screen w-screen"></iframe>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {},
+  };
+};
 
 export default Escape;
