08d20964834b4eed70282f79bde16bafb9e97f75
availe <94802525+availe@users.noreply.github.com>
2024-09-15 17:10:15 -0400

    security headers

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,18 @@ const nextConfig = {
         headers: [
           { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
           { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self'; worker-src 'self'; frame-ancestors 'none';",
+          },
+        ],
+      },
+      {
+        source: "/Escape HTML/:path*",
+        headers: [
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
         ],
       },
     ];
