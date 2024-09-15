03ee9aa92469277a742c6cfe36e3cea68f13a23f
availe <94802525+availe@users.noreply.github.com>
2024-09-15 18:24:00 -0400

    Revert "ssr render"
    This reverts commit 2cf1c972f1f057ce419d1c2401f3dceeedbf060b.

--- src/app/projects/escape/page.tsx
+++ src/app/projects/escape/page.tsx
@@ -1,15 +1,7 @@
-import { GetServerSideProps } from "next";
-
 const Escape = () => {
-  return (
-    <iframe src="/escape-html/game.html" className="h-screen w-screen"></iframe>
-  );
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    props: {},
-  };
-};
+    return (
+        <iframe src="/escape-html/game.html" className="h-screen w-screen"></iframe>
+    );
+}
 
 export default Escape;
