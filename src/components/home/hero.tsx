a24020da843b5c2b0e0a4e5cdab1c8a1c84d220b
availe <94802525+availe@users.noreply.github.com>
2024-09-26 18:10:01 -0400

    test

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
-    <main className="min-h-[600px] min-h-[85svh] bg-[url('/flat-mountains.svg')] bg-cover bg-center flex flex-col justify-center">
+    <main className="min-h-[600px] bg-[url('/flat-mountains.svg')] bg-cover bg-center flex flex-col justify-center">
       <section className="text-center flex flex-col gap-6 md:gap-8 md:mt-[-10svh]">
         <div className="flex flex-col gap-2">
           <h1 className="text-3xl md:text-5xl">Hello, I&apos;m Rafael Diaz</h1>
