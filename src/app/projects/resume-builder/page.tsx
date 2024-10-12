e55bd938d6eee5552627b0de2c64a3be54e1f139
availe <94802525+availe@users.noreply.github.com>
2024-10-12 17:52:39 -0400

    textarea

---
+++ src/app/projects/resume-builder/page.tsx
@@ -0,0 +1,9 @@
+import { Textarea } from "@/components/ui/textarea";
+
+const ResumeBuilder = () => {
+    return (
+    <Textarea placeholder="Type your message here." />
+    );
+}
+
+export default ResumeBuilder;
