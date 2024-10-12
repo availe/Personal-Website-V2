c1d6606802d0c7e524eef61af0f3f63924c006cf
availe <94802525+availe@users.noreply.github.com>
2024-10-12 17:56:16 -0400

    Revert "textarea"
    This reverts commit e55bd938d6eee5552627b0de2c64a3be54e1f139.

--- src/app/projects/resume-builder/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Textarea } from "@/components/ui/textarea";
-
-const ResumeBuilder = () => {
-    return (
-    <Textarea placeholder="Type your message here." />
-    );
-}
-
-export default ResumeBuilder;
