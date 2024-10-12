5322f80e85df4f4582fc58a979a863d9c8aec47f
availe <94802525+availe@users.noreply.github.com>
2024-10-12 19:16:00 -0400

    restore project page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,14 @@
+import ProjectEntry from "@/components/project-entry/project-entry";
+import ProjectsData from "@/data/txt/project-data";
+
+const ProjectsPage = () => {
+  return (
+    <div className="container mx-auto flex flex-col gap-12 pt-2">
+      {ProjectsData.map((project, index) => (
+        <ProjectEntry key={index} {...project} preload={index < 2} />
+      ))}
+    </div>
+  );
+};
+
+export default ProjectsPage;
